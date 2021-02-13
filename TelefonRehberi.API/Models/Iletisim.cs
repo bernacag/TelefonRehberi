83bb8d16c3dcae89fc53a9f4b1c6624169a7f824
bernacag <bernacaglarrr@gmail.com>
2021-02-13 23:35:32 +0300

    Modeller eklendi

---
+++ TelefonRehberi.API/Models/Iletisim.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelefonRehberi.API.Models
+{
+    public enum BilgiTip
+    {
+        Telefon,
+        Email,
+        Konum
+    }
+
+    public class Iletisim
+    {
+        public int Id { get; set; }
+
+        public BilgiTip BilgiTipi { get; set; }
+
+        public string BilgiIcerigi { get; set; }
+
+    }
+}
---
+++ TelefonRehberi.API/Models/Kisi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelefonRehberi.API.Models
+{
+    public class Kisi
+    {
+        public int Id { get; set; }
+
+        public int UUID { get; set; }
+
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string  Firma { get; set; }
+
+        public bool SilindiMi { get; set; }
+
+
+    }
+}
